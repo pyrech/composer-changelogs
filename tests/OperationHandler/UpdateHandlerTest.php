6f26bbb7ed83711ca1043a1e61da24fad916e420
Micheal Mand <micheal@kmdwebdesigns.com>
2021-10-12 15:22:43 -0600

    Add another test to cover major output color too
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>

--- tests/OperationHandler/UpdateHandlerTest.php
+++ tests/OperationHandler/UpdateHandlerTest.php
@@ -197,6 +197,47 @@ class UpdateHandlerTest extends TestCase
         );
     }
 
+    public function testItOutputsTheCorrectSemverColors()
+    {
+        $base = new Package('acme/my-project1', 'v1.0.0.0', 'v1.0.0');
+        $patch = new Package('acme/my-project1', 'v1.0.1.0', 'v1.0.1');
+        $minor = new Package('acme/my-project2', 'v1.1.0.0', 'v1.1.0');
+        $major = new Package('acme/my-project2', 'v2.0.0.0', 'v2.0.0');
+
+        $patchUpdate = new UpdateOperation($base, $patch);
+
+        $expectedOutput = [
+            ' - <fg=green>acme/my-project1</fg=green> updated from <fg=yellow>v1.0.0</fg=yellow> to <fg=yellow>v1.0.1</fg=yellow> <fg=cyan>patch</>',
+        ];
+
+        $this->assertSame(
+            $expectedOutput,
+            $this->SUT->getOutput($patchUpdate, null)
+        );
+
+        $minorUpdate = new UpdateOperation($base, $minor);
+
+        $expectedOutput = [
+            ' - <fg=green>acme/my-project1</fg=green> updated from <fg=yellow>v1.0.0</fg=yellow> to <fg=yellow>v1.1.0</fg=yellow> <fg=magenta>minor</>',
+        ];
+
+        $this->assertSame(
+            $expectedOutput,
+            $this->SUT->getOutput($minorUpdate, null)
+        );
+
+        $majorUpdate = new UpdateOperation($base, $major);
+
+        $expectedOutput = [
+            ' - <fg=green>acme/my-project1</fg=green> updated from <fg=yellow>v1.0.0</fg=yellow> to <fg=yellow>v2.0.0</fg=yellow> <fg=red>major</>',
+        ];
+
+        $this->assertSame(
+            $expectedOutput,
+            $this->SUT->getOutput($majorUpdate, null)
+        );
+    }
+
     public function testItDisplaysVcsRevisionForDevPackage()
     {
         $package1 = new Package('acme/my-project1', 'dev-master', 'dev-master');
